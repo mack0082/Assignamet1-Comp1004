3f94675f5ac2499daff85016b34b4927aa5eef13
mack0082 <mack.waters0803@gmail.com>
2017-01-23 19:15:31 -0500

    first commit

---
+++ Assignment1-1004/Mail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1_1004
+{
+    public partial class Mail : Form
+    {
+        public Mail()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
